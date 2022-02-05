f6dc4a712cd5f1f662b0e6209bfb4c875e6bc057
Jhonatan Nobrega <jhonatan_nobrega@hotmail.com>
2022-02-04 21:21:50 -0300

    Componente header, styles..

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,18 @@
+import { SingInButton } from '../SingInButton';
 import styles from './styles.module.scss';
+import Image from 'next/image';
+import logoImg from '../../../public/images/logo.svg';
 
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="Logo ig.news" />
+        <Image src={logoImg} alt="Logo ig.news" />
         <nav>
           <a href="#" className={styles.active}>Home</a>
           <a href="">Posts</a>
         </nav>
+        <SingInButton/>
       </div>
     </header>
   )
